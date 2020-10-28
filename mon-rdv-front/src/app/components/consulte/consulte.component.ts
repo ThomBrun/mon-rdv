5cbfb5b93c3837739ab22469b8512aad05f225da
Thomas <darkanicl@gmail.com>
2020-10-28 17:29:00 +0100

    components

---
+++ mon-rdv-front/src/app/components/consulte/consulte.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-consulte',
+  templateUrl: './consulte.component.html',
+  styleUrls: ['./consulte.component.scss']
+})
+export class ConsulteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ mon-rdv-front/src/app/components/rdv-patient/rdv-patient.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-rdv-patient',
+  templateUrl: './rdv-patient.component.html',
+  styleUrls: ['./rdv-patient.component.scss']
+})
+export class RdvPatientComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
