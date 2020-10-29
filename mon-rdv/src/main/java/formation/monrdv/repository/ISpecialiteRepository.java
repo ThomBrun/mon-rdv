f036339aed5b9d19a06bdfbca42d017633eecc2b
Thomas <darkanicl@gmail.com>
2020-10-29 13:22:59 +0100

    décommenté

--- mon-rdv/src/main/java/formation/monrdv/repository/ISpecialiteRepository.java
+++ mon-rdv/src/main/java/formation/monrdv/repository/ISpecialiteRepository.java
@@ -1,6 +1,10 @@
 package formation.monrdv.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import formation.monrdv.model.Specialite;
 
